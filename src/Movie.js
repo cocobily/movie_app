b2aba79fc475e38696461681312c9cc16aaef25d
cocobily <inheehappy@gmail.com>
2017-12-28 17:42:39 +0900

    출처남김

--- src/Movie.js
+++ src/Movie.js
@@ -3,37 +3,6 @@ import PropTypes from 'prop-types';
 import LinesEllipsis from 'react-lines-ellipsis'
 import './Movie.css';
 
-/*
-class Movie extends Component {
-
-    static propTypes ={
-        title : PropTypes.string.isRequired,
-        poster : PropTypes.string.isRequired
-    }
-
-    render() {
-        return (
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
-}
-
-class MoviePoster extends Component{
-    static propTypes ={
-        poster : PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <img src={this.props.poster} />
-        )
-    }
-}
-*/
-
 function Movie({title, poster, genres, synopsis}){
     return (
         <div className="Movie">
